9ecaa57df67315ead0b09e9a98c3baa0b96b81c4
brumzis <brumzis@wgu.edu>
2022-03-07 09:26:01 -0500

    main menu screen added

--- src/Controller/login_screen.java
+++ src/Controller/login_screen.java
@@ -3,13 +3,19 @@ package Controller;
 import Model.JDBC;
 import com.sun.scenario.effect.impl.sw.java.JSWBlend_SRC_OUTPeer;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.w3c.dom.ls.LSOutput;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -46,13 +52,20 @@ public class login_screen implements Initializable {
 
     }
 
-    public void onLoginButtonClick(ActionEvent actionEvent) throws SQLException {
+    public void onLoginButtonClick(ActionEvent actionEvent) throws SQLException, IOException {
         String userNameInput = userIDTextBox.getText();
         String passwordInput = passwordTextBox.getText();
         int userID = getUserID(userNameInput);
 
-        if(userID != 0 && (validPassword(getUserID(userNameInput), passwordInput)))
+        if(userID != 0 && (validPassword(getUserID(userNameInput), passwordInput))) {
             System.out.println("username and password are good - go to main page");
+            Parent root = FXMLLoader.load(getClass().getResource("/view/main_menu.fxml"));
+            Stage menuStage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+            Scene menuScene = new Scene(root, 600, 400);
+            menuStage.setTitle("Main Menu");
+            menuStage.setScene(menuScene);
+            menuStage.show();
+        }
         else
             loadPasswordErrorBox();
 
---
+++ src/Controller/main_menu.java
@@ -0,0 +1,31 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class main_menu {
+    public Button customerAddButton;
+    public Button customerUpdateButton;
+    public Button customerDeleteButton;
+    public Button appointmentNewButton;
+    public Button appointmentEditButton;
+    public Button appointmentDeleteButton;
+
+    public void addCustomerButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void updateCustomerButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void deleteCustomerButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void newApptButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void editApptButtonClick(ActionEvent actionEvent) {
+    }
+
+    public void deleteApptButtonClick(ActionEvent actionEvent) {
+    }
+}
