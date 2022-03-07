2d9269873bd2b9d5528930bc31e2d7f37cd1b5bd
brumzis <brumzis@wgu.edu>
2022-03-07 13:06:22 -0500

    new customer screen look/feel done

--- src/Controller/add_customer.java
+++ src/Controller/add_customer.java
@@ -1,4 +1,13 @@
 package Controller;
 
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class add_customer {
+    public TextField customerNameTbox;
+    public TextField customerAddressTbox;
+    public TextField postalCodeTbox;
+    public TextField phoneNumberTbox;
+    public ComboBox countryComboBox;
+    public ComboBox divisionComboBox;
 }
