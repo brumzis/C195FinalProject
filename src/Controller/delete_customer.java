0589bd6c8b0fa116e1dbdf71e73bc1bef21b38a5
brumzis <brumzis@wgu.edu>
2022-03-07 15:50:48 -0500

    delete screen started

---
+++ src/Controller/delete_customer.java
@@ -0,0 +1,29 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class delete_customer {
+    public TextField deleteCustomerIDTbox;
+    public Button deleteCustomerButton;
+    public Button deleteCustomerCancelButton;
+
+    public void cancelButtonClick(ActionEvent actionEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/main_menu.fxml"));
+        Stage menuStage = (Stage)deleteCustomerCancelButton.getScene().getWindow();
+        Scene menuScene = new Scene(root, 600, 400);
+        menuStage.setTitle("Main Menu");
+        menuStage.setScene(menuScene);
+        menuStage.show();
+    }
+
+    public void deleteButtonClick(ActionEvent actionEvent) {
+    }
+}
